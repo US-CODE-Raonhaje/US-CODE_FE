2c7914c6d3ed6c72382c95e216ce098fb96f74eb
hwido <kingluminance@gmail.com>
2025-06-28 11:13:38 +0900

    [feat]수정 페이지 만듬

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Home from "./pages/Home";
 import MapDetail from "./pages/MapDetail";
-import Frame from "./components/Frame";
 import MyPage from "./pages/MyPage";
 import LoginPage from "./pages/login/LoginPage";
 import LoginProfilePage from "./pages/login/LoginProfilePage";
@@ -10,10 +9,11 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import MyPosts from "./pages/MyPosts";
 
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Post from "./pages/Post";
 import Notification from "./pages/Notification";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   const [storyCount, setStoryCount] = useState(0);
@@ -23,37 +23,31 @@ function App() {
   };
 
   return (
-    <Frame>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <ProtectedRoute>
-              <LoginPage />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route path="/login/profile" element={<LoginProfilePage />}></Route>
-        <Route path="/login/location" element={<LoginLocationPage />}></Route>
-        <Route path="/" element={<Home storyCount={storyCount} />} />
-
-        <Route
-          path="/post"
-          element={<Post onPostSubmit={handlePostSubmit} />}
-        />
-
-        <Route path="/map" element={<MapDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/notification" element={<Notification />} />
-        <Route path="/map" element={<MapDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route
-          path="/kakao-redirect"
-          element={<KakaoRedirectPage></KakaoRedirectPage>}
-        />
-        <Route path="mypage/posts" element={<MyPosts />}></Route>
-      </Routes>
-    </Frame>
+    <Routes>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/login/profile" element={<LoginProfilePage />}></Route>
+      <Route path="/login/location" element={<LoginLocationPage />}></Route>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home storyCount={storyCount} />{" "}
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/post" element={<Post onPostSubmit={handlePostSubmit} />} />
+      <Route path="/map" element={<MapDetail />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/notification" element={<Notification />} />
+      <Route path="/map" element={<MapDetail />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route
+        path="/kakao-redirect"
+        element={<KakaoRedirectPage></KakaoRedirectPage>}
+      />
+      <Route path="/mypage/posts" element={<MyPosts />}></Route>
+      <Route path="/mypage/posts/:idx" element={<PostDetail />}></Route>
+    </Routes>
   );
 }
 
