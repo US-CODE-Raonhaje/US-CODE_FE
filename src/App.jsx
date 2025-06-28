4bc0ef64c3acff79b8ffe2c3daab9ac917fa1d87
hwido <kingluminance@gmail.com>
2025-06-29 03:24:02 +0900

    작업 중인 변경사항 임시 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import MapDetail from './pages/MapDetail';
-import Frame from './components/Frame';
-import MyPage from './pages/MyPage';
-import Post from './pages/Post';
-import Notification from './pages/Notification';
+import Home from "./pages/Home";
+import MapDetail from "./pages/MapDetail";
+import Frame from "./components/Frame";
+import MyPage from "./pages/MyPage";
+import Post from "./pages/Post";
+import Notification from "./pages/Notification";
 
 function App() {
   const [storyCount, setStoryCount] = useState(0);
@@ -16,19 +16,20 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <Frame>
-        <Routes>
-          <Route path="/" element={<Home storyCount={storyCount} />} />
-          
-          <Route path="/post" element={<Post onPostSubmit={handlePostSubmit} />} />
-          
-          <Route path="/map" element={<MapDetail />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/notification" element={<Notification />} />
-        </Routes>
-      </Frame>
-    </BrowserRouter>
+    <Frame>
+      <Routes>
+        <Route path="/" element={<Home storyCount={storyCount} />} />
+
+        <Route
+          path="/post"
+          element={<Post onPostSubmit={handlePostSubmit} />}
+        />
+
+        <Route path="/map" element={<MapDetail />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/notification" element={<Notification />} />
+      </Routes>
+    </Frame>
   );
 }
 
